4c7517cd84547106382c5af7bf3734cf7f3642eb
Emanuel Asante Amponsah <emanuelasante80@gmail.com>
2026-04-07 17:10:41 +0100

    Delete Backend directory

--- Backend/Routes/users.route.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import {  register, login } from "../Controller/users.controller.js";
-
-const router = express.Router();
-
-router.post("/login", login);
-router.post("/register", register);
-
-export default router;
